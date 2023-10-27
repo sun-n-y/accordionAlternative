eb9b445bb2ae1134d27223e71bc2746d7a9165e1
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-27 11:33:52 -0400

    add accordian alternative complete

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,11 +1,18 @@
 import Question from './Question';
 
-const Questions = ({ questions }) => {
+const Questions = ({ questions, activeId, toggleQuestion }) => {
   return (
     <section className="container">
       <h1>questions</h1>
       {questions.map((question) => {
-        return <Question {...question} key={question.id} />;
+        return (
+          <Question
+            {...question}
+            key={question.id}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          />
+        );
       })}
     </section>
   );
